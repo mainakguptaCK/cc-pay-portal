e28be40d2b399de270c885e2b642b1a652021df1
shaswata <shaswata.das@cloudkaptan.com>
2025-06-03 14:03:20 +0530

    modify it with app service url

--- src/pages/admin/UserManagement.tsx
+++ src/pages/admin/UserManagement.tsx
@@ -46,7 +46,7 @@ const UserManagement: React.FC = () => {
   useEffect(() => {
     const fetchUserCards = async (userId: string) => {
       try {
-        const response = await fetch('http://127.0.0.1:5000/api/card/getCardDetails', {
+        const response = await fetch('https://cc-pay-app-service-dev-cecxemfggbf0dzas.eastus-01.azurewebsites.net/api/card/getCardDetails', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ UserID: userId })
